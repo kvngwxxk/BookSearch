b3eb76d1c89c6acdc41524aac3ec1473bbc7e9fd
kangwooklee <kngwk.bsns@etoos.com>
2022-11-23 16:50:15 +0900

    DetailViewController 구현 naver & kakao api 연동

--- BookSearch/ViewControllers/DetailViewController.swift
+++ BookSearch/ViewControllers/DetailViewController.swift
@@ -12,7 +12,38 @@ import SnapKit
 class DetailViewController: UIViewController {
 	let naverBook: NaverBook?
 	let kakaoBook: KakaoBook?
-	var thumbnail = UIImageView()
+	
+	let backButton: UIButton = {
+		let btn = UIButton(type: .close)
+		btn.translatesAutoresizingMaskIntoConstraints = false
+		return btn
+	}()
+	let thumbnail = UIImageView()
+	let titleLabel: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.numberOfLines = 0
+		label.lineBreakMode = .byTruncatingTail
+		label.font = UIFont.boldSystemFont(ofSize: 16)
+		return label
+	}()
+	let authorLabel: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.numberOfLines = 0
+		label.lineBreakMode = .byTruncatingTail
+		label.font = UIFont.systemFont(ofSize: 14)
+		return label
+	}()
+	let publisherLabel: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.numberOfLines = 0
+		label.lineBreakMode = .byTruncatingTail
+		label.font = UIFont.systemFont(ofSize: 12)
+		return label
+	}()
+	
 	var topData = [String: String]()
 	var bottomKeyData = [String]()
 	var bottomValueData = [String]()
@@ -26,9 +57,11 @@ class DetailViewController: UIViewController {
 		tableView.registerCell(cellType: DetailViewCell.self, reuseIdentifier: "DetailViewCell")
 		tableView.rowHeight = UITableView.automaticDimension
 		tableView.estimatedRowHeight = 200
+		backButton.addTarget(self, action: #selector(closeView), for: .touchUpInside)
 		setData()
 		setAutoLayout()
 		setThumbnail()
+		setLabels()
 	}
 	
 	init(naverBook: NaverBook? = nil, kakaoBook: KakaoBook? = nil) {
@@ -58,7 +91,7 @@ class DetailViewController: UIViewController {
 			let publisher = kakaoBook.publisher.isEmpty ? "출판사 없음" : kakaoBook.publisher
 			topData.updateValue(thumbnail, forKey: "thumbnail")
 			topData.updateValue(title, forKey: "title")
-			topData.updateValue(authors, forKey: "authors")
+			topData.updateValue(authors, forKey: "author")
 			topData.updateValue(publisher, forKey: "publisher")
 			
 			
@@ -99,7 +132,32 @@ class DetailViewController: UIViewController {
 			
 		}  else if let naverBook = naverBook {
 			// MARK: Naver Book Data
+			// 상단 항목
+			let thumbnail = naverBook.image.isEmpty ? "" : naverBook.image
+			let title = naverBook.title.isEmpty ? "제목 없음" : naverBook.title
+			let author: String = naverBook.author.isEmpty ? "작자 미상" : naverBook.author
+			let publisher = naverBook.publisher.isEmpty ? "출판사 없음" : naverBook.publisher
+			topData.updateValue(thumbnail, forKey: "thumbnail")
+			topData.updateValue(title, forKey: "title")
+			topData.updateValue(author, forKey: "author")
+			topData.updateValue(publisher, forKey: "publisher")
 			
+			// 하단 항목
+			bottomKeyData.append("pubDate")
+			bottomKeyData.append("isbn")
+			bottomKeyData.append("discount")
+			bottomKeyData.append("link")
+			bottomKeyData.append("description")
+			let description = naverBook.description.isEmpty ? "소개 없음" : naverBook.description
+			let pubDate = naverBook.pubDate.isEmpty ? "출판 년도 미상" : naverBook.pubDate
+			let isbn = naverBook.isbn.isEmpty ? "isbn 없음" : naverBook.isbn
+			let discount = naverBook.discount.isEmpty ? "절판으로 인한 가격 미표기" : naverBook.discount
+			let link = naverBook.link.isEmpty ? "url 없음" : naverBook.link
+			bottomValueData.append(pubDate)
+			bottomValueData.append(isbn)
+			bottomValueData.append(discount)
+			bottomValueData.append(link)
+			bottomValueData.append(description)
 		}
 	}
 	private func setThumbnail() {
@@ -119,10 +177,26 @@ class DetailViewController: UIViewController {
 		} else {
 			print("img url is nil")
 		}
+		thumbnail.layer.borderColor = UIColor.black.cgColor
+		thumbnail.layer.borderWidth = 0.5
+	}
+	private func setLabels() {
+		let title = topData["title"]
+		let author = topData["author"] ?? "미상"
+		let publisher = topData["publisher"] ?? "출판사 없음"
+		
+		titleLabel.text = title
+		authorLabel.text = "저자 : \(author)"
+		publisherLabel.text = "출판사 : \(publisher)"
 	}
 	private func setAutoLayout() {
 		self.view.addSubview(tableView)
 		self.view.addSubview(thumbnail)
+		self.view.addSubview(backButton)
+		self.view.addSubview(titleLabel)
+		self.view.addSubview(authorLabel)
+		self.view.addSubview(publisherLabel)
+		thumbnail.translatesAutoresizingMaskIntoConstraints = false
 		
 		tableView.snp.makeConstraints { make in
 			make.top.equalTo(self.view.snp.centerY).offset(-100)
@@ -130,13 +204,43 @@ class DetailViewController: UIViewController {
 			make.leading.equalTo(self.view)
 			make.trailing.equalTo(self.view)
 		}
+		
 		thumbnail.snp.makeConstraints { make in
-			make.top.equalTo(self.view).offset(20)
+			make.top.equalTo(backButton.snp.bottom).offset(20)
 			make.bottom.equalTo(tableView.snp.top)
 			make.leading.equalTo(self.view).offset(20)
 			make.width.equalTo(self.view.frame.width * 2 / 5)
 		}
+		backButton.snp.makeConstraints { make in
+			make.top.equalTo(self.view).offset(20)
+			make.trailing.equalTo(self.view).offset(-20)
+			make.width.equalTo(20)
+			make.height.equalTo(20)
+		}
+		titleLabel.snp.makeConstraints { make in
+			make.top.equalTo(backButton.snp.bottom).offset(15)
+			make.bottom.equalTo(authorLabel.snp.top).offset(-20)
+			make.leading.equalTo(thumbnail.snp.trailing).offset(20)
+			make.trailing.equalTo(self.view).offset(-20)
+		}
 		
+		authorLabel.snp.makeConstraints { make in
+			make.top.equalTo(titleLabel.snp.bottom).offset(30)
+			make.bottom.equalTo(publisherLabel.snp.top).offset(-20)
+			make.leading.equalTo(thumbnail.snp.trailing).offset(20)
+			make.trailing.equalTo(self.view).offset(-20)
+		}
+		
+		publisherLabel.snp.makeConstraints { make in
+			make.top.equalTo(authorLabel.snp.bottom).offset(30)
+			make.leading.equalTo(thumbnail.snp.trailing).offset(20)
+			make.trailing.equalTo(self.view).offset(-20)
+			
+		}
+	}
+	
+	@objc func closeView() {
+		self.presentingViewController?.dismiss(animated: true, completion: nil)
 	}
 }
 
